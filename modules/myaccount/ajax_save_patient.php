eeba9405d0edb445ca860a6c4d22bc6c518485a4
lordjimmy <lordjimmyii@gmail.com>
2013-02-16 14:02:00 -0500

    The eval_id is generated as a session for save in the database. TODO: destroy the SESSION['evaluation']

--- modules/myaccount/ajax_save_patient.php
+++ modules/myaccount/ajax_save_patient.php
@@ -21,7 +21,19 @@
     header('Location: ../myaccount/myaccount.php?page=patients'); 
   }else{
     mysql_query("INSERT INTO main_patient (patient_id, timestamp, name, surn, gender, birthd, countrybth, citybth, statebth, digiter_id) VALUES ('".$docidnum."', '".$actual_date."', '".$name."', '".$surname."', '".$gender."', '".$birthd."', '".$countrybth."', '".$citybth."', '".$statebth."', '".$user_id."')");
-    
+
+		/// In this part when the patient is add to the data base, also it's recorded the main_eval, to keep the conection between the doctor and the patient 
+		
+		$digiter_id = $_SESSION['username'];
+		mysql_query("INSERT INTO main_eval (patient_id, digiter_id) VALUES ('$docidnum', '$digiter_id');"); 
+		
+		// Search for the generated ID
+
+		$search_sql=("SELECT eval_id FROM main_eval WHERE patient_id = '$docidnum' AND digiter_id = '$digiter_id'");
+		$search_result = mysql_query($search_sql);
+		$eval_id_arr = mysql_fetch_array($search_result);
+		$_SESSION['evaluation'] = $eval_id_arr[0];
+
     $patient = $docidnum."-".$name."-".$surname."-";
     $patient .= $gender."-".$birthd."-".$countrybth."-";
     $patient .= $citybth."-".$statebth."-".$user_id;
