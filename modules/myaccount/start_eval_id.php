e5efe1b4ab4199655985faafebd196161d9a6b18
Alejandro Toro <joaleto@yahoo.com>
2013-02-17 00:52:20 -0500

    Session 'evaluation' asigned

---
+++ modules/myaccount/start_eval_id.php
@@ -0,0 +1,25 @@
+<?php 
+include '../DB/connect.php';
+session_start();
+if (!isset($_SESSION['hap_patient_id'])||!isset($_SESSION['username'])) {
+	header("location:myaccount.php?page=patients");
+}
+else {
+$docidnum=$_SESSION['hap_patient_id'];
+$digiter_id = $_SESSION['username'];
+
+//adding event to table main eval
+mysql_query("INSERT INTO main_eval (patient_id, digiter_id) VALUES ('$docidnum', '$digiter_id');");
+
+//asjusting session variable for unique main eval id
+$search_sql=("SELECT eval_id FROM main_eval WHERE patient_id = '$docidnum' AND digiter_id = '$digiter_id'");
+$search_result = mysql_query($search_sql);
+$eval_id_arr = mysql_fetch_array($search_result);
+
+//asigning value to session, same variable as in file 'ajax_save_patient.php' in line 37 or near
+$_SESSION['evaluation'] = $eval_id_arr[0];
+
+//redirect to the main form
+header('Location: myaccount.php?page=basic'); 
+}
+?>
