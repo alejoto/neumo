eb2657572103a12128f3c094aebfa39a688afa28
lordjimmy <lordjimmyii@gmail.com>
2013-02-18 11:46:57 -0500

    The form in prueba esfuerzo cp persist in the database in the table hap_cp_stress_test

--- modules/patient/cardiovascular/cp_test.php
+++ modules/patient/cardiovascular/cp_test.php
@@ -7,13 +7,13 @@
   <div class="row">
     <div class="span2" style="text-align:right">Fecha</div>
     <div class="span3">
-      <input type="text" id="y_cpst" class="span1" placeholder="a&ntilde;o"/>
-      <input type="text" id="m_cpst" class="span1" placeholder="mes"/>
-      <input type="text" id="d_cpst" class="span1" placeholder="d&iacute;a"/>
+      <input type="text" id="y_cpst" name="cp_stress_date" class="span1 cpt date1" placeholder="a&ntilde;o"/>
+      <input type="text" id="m_cpst" class="span1 cpt date1" placeholder="mes"/>
+      <input type="text" id="d_cpst" class="span1 cpt date1" placeholder="d&iacute;a"/>
     </div>
     <div class="span1" style="text-align:right">VO2 max</div>
     <div class="span1">
-      <input type="text" class="span1" id="vo2max_peak" placeholder="mL/K/m">
+      <input type="text" class="span1 cpt" id="vo2max_peak" placeholder="mL/K/m" name="vo2max_peak">
     </div>
   </div>
   <div class="row">
@@ -21,16 +21,16 @@
       PA en VO2max
     </div>
     <div class="span1">
-      <input type="text" class="span1" id="pa_sist" placeholder="sist">
+      <input type="text" class="span1 cpt join2" id="pa_sist" placeholder="sist" name="art_press_vo2">
     </div>
     <div class="span1">
-      <input type="text" class="span1" id="pa_dias" placeholder="diast">
+      <input type="text" class="span1 cpt join2" id="pa_dias" placeholder="diast">
     </div>
     <div class="span2" style="text-align:right">
       Pulso en VO2max
     </div>
     <div class="span1">
-      <input type="text" class="span1" id="pulse_vo2" placeholder="lat/min">
+      <input type="text" class="span1 cpt" id="pulse_vo2" placeholder="lat/min" name="pulse_vo2">
     </div>
   </div>
   <div class="row">
